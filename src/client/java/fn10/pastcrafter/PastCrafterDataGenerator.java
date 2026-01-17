79c498441fa9f5f520def802287ed8fcdf924a58
_FN10_ <mathdudex09@gmail.com>
2026-01-17 15:54:53 -0400

    init

---
+++ src/client/java/fn10/pastcrafter/PastCrafterDataGenerator.java
@@ -0,0 +1,11 @@
+package fn10.pastcrafter;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PastCrafterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
